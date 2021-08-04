6dc30561d8b11b1071fb0547d1791aaf774c710b
Corenlix <corenlix@gmail.com>
2021-08-05 01:04:54 +0300

    Changed death animation

--- Assets/Scripts/SpawnObjectAfterDeath.cs
+++ Assets/Scripts/SpawnObjectAfterDeath.cs
@@ -4,11 +4,17 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 
+[RequireComponent(typeof(Health))]
 public class SpawnObjectAfterDeath : MonoBehaviour
 {
     [SerializeField] private GameObject newObject;
+    
+    private void OnEnable()
+    {
+        GetComponent<Health>().onDied?.AddListener(Spawn);
+    }
 
-    private void OnDestroy()
+    private void Spawn()
     {
         Instantiate(newObject, transform.position, transform.rotation);
     }
