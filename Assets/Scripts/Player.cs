f39cb13026687a5e0030b6fec0c29c79c871f5fc
Corenlix <corenlix@gmail.com>
2021-08-18 13:25:49 +0300

    Added player input

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,10 +7,9 @@ using UnityEngine.Serialization;
 [RequireComponent(typeof(VelocityMover))]
 public class Player : MonoBehaviour
 {
-    [SerializeField] private PlayerAbility activeAbility;
     [SerializeField] private WeaponsControl weaponsControl;
-    [FormerlySerializedAs("ammoController")] [SerializeField] private AmmoBelt ammoBelt;
-    private Camera _mainCamera;
+    [SerializeField] private AmmoBelt ammoBelt;
+    
 
     public void AddAmmo(AmmoType ammoType, int ammoCount)
     {
@@ -20,33 +19,6 @@ public class Player : MonoBehaviour
     public void AddWeapon(WeaponStats weaponStats)
     {
         weaponsControl.AddWeapon(weaponStats);
-    }
-    
-    private void Update()
-    {
-        InputControls();
-    }
-
-    private void InputControls()
-    {
-        var mouseWorldPosition = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
-        RotateHelper.FlipBodyToPosition(transform, mouseWorldPosition);
-        weaponsControl.RotateSelectedWeaponToTarget(mouseWorldPosition);
-
-        if (Input.GetMouseButton(0))
-        {
-            weaponsControl.Attack(mouseWorldPosition);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Q) || Input.GetAxis("Mouse ScrollWheel") != 0)
-            weaponsControl.SwapWeapon();
-        
-        if(Input.GetMouseButton(1))
-            activeAbility.TryApplyAbility(mouseWorldPosition);
-    }
-    private void Awake()
-    {
-        _mainCamera = Camera.main;
     }
 
     private void OnEnable()
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Helpers;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    [SerializeField] private WeaponsControl weaponsControl;
+    [SerializeField] private PlayerAbility activeAbility;
+    private Camera _mainCamera;
+    
+    private void Awake()
+    {
+        _mainCamera = Camera.main;
+    }
+    private void Update()
+    {
+        InputControls();
+    }
+
+    private void InputControls()
+    {
+        var mouseWorldPosition = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        RotateHelper.FlipBodyToPosition(transform, mouseWorldPosition);
+        weaponsControl.RotateSelectedWeaponToTarget(mouseWorldPosition);
+
+        if (Input.GetMouseButton(0))
+        {
+            weaponsControl.Attack(mouseWorldPosition);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q) || Input.GetAxis("Mouse ScrollWheel") != 0)
+            weaponsControl.SwapWeapon();
+        
+        if(Input.GetMouseButton(1))
+            activeAbility.TryApplyAbility(mouseWorldPosition);
+    }
+}
